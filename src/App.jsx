ee6ba8f381c0ec3a795db1cdeea28de0a6eaf982
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2025-02-15 23:19:55 +0000

    style desktop and mobile

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,22 @@ function App() {
   return (
     <main>
       <section id='product-cards-container'>
-        <div className="product-card">
+        <article className="product-card">
           <div className="product-card-image">
             <picture>
               <source media='(min-width: 768px)' srcSet={productImgDesktop} />
               <source media='(max-width: 767px)' srcSet={productImgMobile} />
-              <img src={productImgMobile} alt='Perfume bottle' />
+              <img src={productImgMobile} loading="lazy" alt='A Chanel perfume bottle lying on its side with the logo facing upward, surrounded by leaves.' />
             </picture>
           </div>
           <div className="product-card-info">
             <small className="card-label">Perfume</small>
             <h5 className="card-title">Gabrielle Essence Eau De Parfum</h5>
             <p className="card-desc">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
-            <p><span className='card-price'>$149.99</span><span className='card-old-price'>$169.99</span></p>
-            <button type="button" className='card-add-to-cart'>Add to Cart</button>
+            <div className='card-price'><span className='card-current-price'>$149.99</span><span className='card-old-price'>$169.99</span></div>
+            <button type="button" className='card-add-to-cart-btn'><span>Add to Cart</span></button>
           </div>
-        </div>
+        </article>
       </section>
     </main>
   )
